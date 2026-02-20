3f80572cb2fcb31df1d84424ea9ffff84e5667a1
Samuel Jenks <mail@samueljenks.me>
2026-02-21 11:41:05 +1300

    revert: verbose logging

--- compiler/plc_driver/src/main.rs
+++ compiler/plc_driver/src/main.rs
@@ -4,10 +4,7 @@ use env_logger::Env;
 
 fn main() {
     //Initialize the logging
-    let env = Env::default()
-        .filter_or("MY_LOG_LEVEL", "trace")
-        .write_style_or("MY_LOG_STYLE", "always");
-
+    let env = Env::default();
     env_logger::init_from_env(env);
 
     let args: Vec<String> = env::args().collect();
